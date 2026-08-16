a15f298de3defb09842a69abb64077ab0b5d921c
Anup <anupgtm07@gmail.com>
2026-08-16 23:42:33 +0545

    new change have been done to repo factory

--- dependencies/repo_factory.py
+++ dependencies/repo_factory.py
@@ -1,70 +1,33 @@
-from typing import Annotated
+from pydantic import EmailStr
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from fastapi import APIRouter, Depends, Request
-from fastapi.responses import JSONResponse
-
-from core.config import settings
-from dependencies.get_user import get_user
-from dependencies.service_factory import get_auth_service
+from core.security import Auth
 from models.user import User
-from schemas.auth_schema import (
-    LoginSchema,
-    RefreshSchema,
-    RegisterCreate,
-    UserReadSchema,
-    UserUpdateSchema,
-)
-from schemas.common import SuccessResponse
-from services.auth_services import AuthService
-
-auth_api = APIRouter(prefix="/auth", tags=["Regsiter endpoints"])
+from schemas.auth_schema import RegisterCreate, UserUpdateSchema
 
 
-@auth_api.post("/register", response_model=SuccessResponse[None])
-async def create_user_endpoint(
-    data: RegisterCreate, service: Annotated[AuthService, Depends(get_auth_service)]
-):
-    return await service.register_service(data)
+class AuthRepo:
+    def __init__(self,session:AsyncSession) -> None:
+        self.db=session
 
+    async def get_user_by_email(self,email:EmailStr):
+        user=(await self.db.execute(select(User).where(User.email==email))).scalar_one_or_none()
+        return user
 
-@auth_api.post("/login")
-async def login_user_endpoint(
-    data: LoginSchema, service: Annotated[AuthService, Depends(get_auth_service)]
-):
-    return await service.login_service(data)
+    async def create_user(self,data:RegisterCreate):
+        user=User(
+            full_name=data.full_name,
+            email=data.email,
+            password=Auth().hash.hash(data.password1)
+        )
+        self.db.add(user)
+        await self.db.commit()
+        return user
 
-
-@auth_api.get("/me", response_model=SuccessResponse[UserReadSchema])
-async def read_user_endpoint(user: Annotated[User, Depends(get_user)]):
-    return SuccessResponse(data=user)
-
-
-@auth_api.patch("/user", response_model=SuccessResponse[None])
-async def update_user_endpoint(
-    data: UserUpdateSchema,
-    user: Annotated[User, Depends(get_user)],
-    auth_service: Annotated[AuthService, Depends(get_auth_service)],
-):
-    return await auth_service.update_user_service(data, user)
-
-
-@auth_api.post("/logout")
-async def logout_user_endpoint():
-    response = JSONResponse(
-        status_code=200,
-        content=SuccessResponse(message="Logout success", data=None).model_dump(),
-    )
-    response.delete_cookie(
-        key=settings.LOGIN_COOKIE_NAME,
-        path=settings.LOGIN_COOKIE_PATH,
-        samesite=settings.SAME_SITE,
-        secure=settings.SECURE,
-    )
-    return response
-
-
-@auth_api.post("/refresh", response_model=SuccessResponse[RefreshSchema])
-async def refresh_user_endpoint(
-    request: Request, auth_service: Annotated[AuthService, Depends(get_auth_service)]
-):
-    return await auth_service.refresh_service(request)
+    async def update_user(self,data:UserUpdateSchema,user:User):
+        for key,value in data.model_dump(exclude_unset=True).items():
+            setattr(user,key,value)
+        await self.db.commit()
+        return True
+sa
