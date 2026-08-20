90a87e19367b1b44d56f3567ce55821fadeef012
Anup <anupgtm07@gmail.com>
2026-08-20 23:58:39 +0545

    repo factory created successfully

--- dependencies/repo_factory.py
+++ dependencies/repo_factory.py
@@ -1,21 +1,10 @@
-from email.mime import base
+from typing import Annotated
+from fastapi import Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+from Expense.service.register import AuthRepo
+from core.db import get_db
+from repo.register import UserRepository
 
-from click import echo
-from sqlalchemy.ext.asyncio import create_async_engine,async_sessionmaker
-from sqlalchemy.orm import declarative_base
-#from core.config import settings
-from Expense.core.db import SessionHandler
-from core.config import settings
 
-#engine build
-engine=create_async_engine(settings.DATABASE_URL,echo=True)
-#session handler
-SessionHandler=async_sessionmaker(
-    bind=engine,#it gives path to datbase
-    expire_on_commit=True, #if datbase row changes then python object(Table row value also changes)
-    autoflush=False #if python object changes then database row doesnt automatically changes we need to change ourself
-)
-base=declarative_base() #base uses the property of declartive base sql alchemcy detects which class is model
-async def get_db():
-    async with SessionHandler() as db:
-        yield db
+def get_auth_repo(db:Annotated[AsyncSession,Depends(get_db)]):
+    return AuthRepo(db)
